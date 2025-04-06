c329f4154297a3b67d800c35580cb964bde524cb
shodan32 <shodan32@gmail.com>
2025-04-06 17:01:59 +0700

    delete adlook banners

--- header.php
+++ header.php
@@ -31,8 +31,6 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
-		<!-- adlook banners -->
-        <script src="https://sdk.adlook.tech/inventory/core.js" async type="text/javascript"></script>
 
         <?php wp_head(); ?> 
 
